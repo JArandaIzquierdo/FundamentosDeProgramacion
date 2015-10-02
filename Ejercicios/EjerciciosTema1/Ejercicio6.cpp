d4d16007cf0e3857bb67c7e82b8c1b92c2cf329e
JArandaIzquierdo <j.arandaizquierdo@gmail.com>
2015-10-02 18:45:47 +0200

    Agregada la solucion del ejercicio 6

---
+++ Ejercicios/EjerciciosTema1/Ejercicio6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+  double cantidadCapital=0, interes=0, capitalFinal=0;
+
+  cout<< "Introduzca el capital a ingresar entre 0 y 100" << endl;
+  cin>> cantidadCapital;
+
+  cout<< "Introduzca un interes" << endl;
+  cin>> interes;
+
+  capitalFinal = cantidadCapital + cantidadCapital * (interes / 100);
+
+  cout<< "El capital obtenido en un año seria de: " << capitalFinal << endl;
+}
