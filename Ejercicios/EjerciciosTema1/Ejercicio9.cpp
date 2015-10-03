5b75bf086a70860786a107cbbb66509497d08181
JArandaIzquierdo <j.arandaizquierdo@gmail.com>
2015-10-03 17:27:30 +0200

    Terminado ejercicio9

---
+++ Ejercicios/EjerciciosTema1/Ejercicio9.cpp
@@ -0,0 +1,23 @@
+/* Seria imposible hacer este ejercicio sin una variable auxiliar, para
+ello he utilizado variableDeCambio*/
+
+#include <iostream>
+using namespace std;
+int main (){
+
+  int edad_Pedro=0, edad_Juan=0, variableDeCambio=0;
+
+  cout<<"Introduzca la edad de Pedro: ";
+  cin>> edad_Pedro;
+
+  cout<< "Introduzca la edad de Juan: ";
+  cin>> edad_Juan;
+
+  variableDeCambio = edad_Pedro;
+  edad_Pedro = edad_Juan;
+  edad_Juan = variableDeCambio;
+
+  cout<< "La edad de Pedro ahora es: "<< edad_Pedro << " y la edad de Juan es: "
+  << edad_Juan << endl;
+
+}
