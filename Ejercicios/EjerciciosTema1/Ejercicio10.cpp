36618b7f096c7d1d69b1b84f15a03186a0b06aad
JArandaIzquierdo <j.arandaizquierdo@gmail.com>
2015-10-05 21:26:39 +0200

    Terminado ejercicio 10 pero no funciona bien

---
+++ Ejercicios/EjerciciosTema1/Ejercicio10.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+
+  double persona1=0, persona2=0, persona3=0, media=0, desviacion=0;
+
+  cout<<  "Introduzca la altura de la primera persona: ";
+  cin>> persona1;
+
+  cout<<  "Introduzca la altura de la segunda persona: ";
+  cin>> persona2;
+
+  cout<<  "Introduzca la altura de la tercera persona: ";
+  cin>> persona3;
+
+  media = (persona1 + persona2 + persona3) * (1/3);
+
+  desviacion = sqrt((1/3) * ( pow((persona1 - media),2) + pow((persona2 - media),2) + pow((persona3 - media),2)));
+
+  cout<< " La media es: "<< media<< endl;
+  cout<< " La desviacion es: "<< desviacion << endl;
+
+}
