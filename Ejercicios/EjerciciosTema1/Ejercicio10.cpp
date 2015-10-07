fcfa5b1ed3d41278024258e8921d2da9ddb3bfe8
JArandaIzquierdo <j.arandaizquierdo@gmail.com>
2015-10-07 23:44:33 +0200

    Arreglada una parte del ejercicio 10

--- Ejercicios/EjerciciosTema1/Ejercicio10.cpp
+++ Ejercicios/EjerciciosTema1/Ejercicio10.cpp
@@ -14,9 +14,9 @@ int main(){
   cout<<  "Introduzca la altura de la tercera persona: ";
   cin>> persona3;
 
-  media = (persona1 + persona2 + persona3) * (1/3);
+  media = (persona1 + persona2 + persona3) * (1.0/3.0);
 
-  desviacion = sqrt((1/3) * ( pow((persona1 - media),2) + pow((persona2 - media),2) + pow((persona3 - media),2)));
+  desviacion = sqrt((1.0/3.0) * ( pow((persona1 - media),2) + pow((persona2 - media),2) + pow((persona3 - media),2)));
 
   cout<< " La media es: "<< media<< endl;
   cout<< " La desviacion es: "<< desviacion << endl;
