d7f0c9ba9eb09c2133b5936534282e5dffff1e4d
JArandaIzquierdo <j.arandaizquierdo@gmail.com>
2015-11-21 17:49:52 +0100

    Terminado ejercicio 19

---
+++ Ejercicios/EjerciciosRelacion2/Ejercicio19.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int anios, nAnios;
+    double capital, interes;
+
+    cout << "Introduce el numero de anios: ";
+    cin >> anios ;
+    cout << "Introduce el interes entre 0 y 100: ";
+    cin >> interes;
+    cout << "Introduce el capital: ";
+    cin >> capital;
+
+    nAnios = 1;
+
+    while(nAnios <= anios ){
+
+            capital = capital +(capital*(interes/100));
+
+            cout << "Total en el anio numero " << " " << nAnios << " " << capital << endl;
+            nAnios ++;
+    }
+}
