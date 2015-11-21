3c0e4f3d5c160853243785f9df672e5caa2fb706
JArandaIzquierdo <j.arandaizquierdo@gmail.com>
2015-11-21 19:24:44 +0100

    terminado ejercicio 32

--- Ejercicios/EjerciciosRelacion2/Ejercicio32.cpp
+++ Ejercicios/EjerciciosRelacion2/Ejercicio32.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
+
 int main(){
 
-      int n,numero,i,contador=0;
+    int entero, digito, narcisista=0, i=0;
+    int modulo, aux, aux2, div =1;
+    double operacion;
 
-      cout << "Intruduzca un numero: ";
-      cin >> n;
+    do{
+    	 cout << "Introduce un entero positivo: ";
+        cin >> entero;
+    }while (entero < 0);
 
-      numero=n;
+    aux = entero;
+    aux2 = entero;
 
-      while(n > 0){
-            n = n%i;
-            i = i*10;
-            contador++;
-      }
+    while(digito !=0 ){
+        aux2 = aux2 / 10;
+        digito = aux2 % 10;
+        i++;
+    }
 
+    for(int j=0; j<i-1; j++)
+        div = div *10;
 
-      cout << "numero: " << numero << endl;
-      cout << "contador: " << contador << endl;
+    while (div !=0){
+        modulo = aux % div;
+        digito = aux / div;
+        operacion = pow(digito,i);
+        narcisista = operacion + narcisista ;
+        div = div / 10;
+        aux = modulo;
+    }
 
+    if(narcisista==entero)
+        cout << "El numero es narcisista"<<endl;
+    else
+        cout << "El numero no es narcisista"<<endl;;
 }
