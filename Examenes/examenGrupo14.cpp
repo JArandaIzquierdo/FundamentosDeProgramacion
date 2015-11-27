35359896a77b71ccac1f4853ee4bc81bf095c6a2
Javier Aranda <j.arandaizquierdo@gmail.com>
2015-11-27 01:31:48 +0100

    Arreglando el ejercicio de examen sin lograr contar los consecutivos

--- Examenes/examenGrupo14.cpp
+++ Examenes/examenGrupo14.cpp
@@ -4,29 +4,35 @@ using namespace std;
 void MostrarNumeroDeVeces(long long numero){
 
       long long int aux;
-      int resto,contador,max=0,cifraMaxima=0; //x
+      int resto,contador,masRepetida=0,consecutivos=0,contador2=0;
 
 
       for (int i=0; i <= 9; i++){
             aux = numero;
             contador=0;
-            for(int j=0; aux > 0; j++){
+
+            while(aux > 0){
                   resto=aux%10;
                   aux = aux/10;
-
+                  cout << resto << endl;
                   if (resto == i){
                         contador++;
+                        consecutivos++;// El problema esta aqui
                   }
+                  else if(contador2 < consecutivos){
+                        contador2=consecutivos;
+                        cout<< "Contador2 = "<<contador2<<endl;
+                        masRepetida=i;
+                        consecutivos=0;
+                  }
+                  cout << "masRepetida es: "<< masRepetida<<endl;
             }
             if (contador > 0){
                   cout << "La cifra "<< i << " se repite "<< contador << " veces"<<endl;
             }
-            if(contador > max){
-                  max = contador;
-                  cifraMaxima = i;
-            }
       }
-cout << "La cifra con el mayor numero de repeticiones es: "<<cifraMaxima<<endl;
+
+cout << "La cifra mas repetida de forma consecutiva es: "<< masRepetida <<endl;
 
 
 }
