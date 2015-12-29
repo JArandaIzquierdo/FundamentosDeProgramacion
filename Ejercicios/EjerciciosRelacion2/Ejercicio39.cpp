12362e7ab27fa7a49bda3f192360878d9327b1ec
Javier Aranda <j.arandaizquierdo@gmail.com>
2015-12-29 14:27:23 +0000

    PEndiente ejercicio 39

--- Ejercicios/EjerciciosRelacion2/Ejercicio39.cpp
+++ Ejercicios/EjerciciosRelacion2/Ejercicio39.cpp
@@ -1,21 +1,56 @@
 #include <iostream>
 using namespace std;
 int main(){
+      int   numero,
+            contador0=0,
+            contador1=0,
+            contador2=0,
+            contador3=0,
+            contador4=0,
+            contador5=0,
+            contador6=0,
+            contador7=0,
+            contador8=0,
+            contador9=0;
 
-      int numero, k, c;
 
-          cout << "Introduzca numeros(-1 para terminar): ";
-          cin >> numero;
+      cout <<"Introduzca un numero: ";
+      cin>> numero;
 
-          while (numero != -1) {
-              k = numero;
-              c = 1;
-              while (numero != -1 && k == numero) {
-                  cin >> numero;
-                  if (k == numero) {
-                      c++;
-                  }
-              }
-              cout << c << " " << k << " ";
-        }
+      while(numero >= 0){
+
+            if(numero == 0){
+                  contador0++;
+            }
+            if(numero == 1){
+                  contador1++;
+            }
+            if(numero == 2){
+                  contador2++;
+            }
+            if(numero == 3){
+                  contador3++;
+            }
+            if(numero == 4){
+                  contador4++;
+            }
+            if(numero == 5){
+                  contador5++;
+            }
+            if(numero == 6){
+                  contador6++;
+            }
+            if(numero == 7){
+                  contador7++;
+            }
+            if(numero == 8){
+                  contador8++;
+            }
+            if(numero == 9){
+                  contador9++;
+            }
+
+            for (int i = 0; i <=9; i++){}
+
+      }
 }
